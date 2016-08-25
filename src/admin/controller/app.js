ab64576fa411eeba4b5fb9ac7133df913bcf5908
eastmoney <eastmoney@eastmoney.com>
2016-08-25 18:49:42 +0800

    update

--- src/admin/controller/app.js
+++ src/admin/controller/app.js
@@ -3,38 +3,40 @@
 import Base from './base.js';
 
 export default class extends Base {
-  /**
-   * index action
-   * @return {Promise} []
-   */
-  async indexAction(){
-
-    const data = await this.model('appinfo').select();
-    this.assign('data',data);
-    return this.display();
-  }
-
-  addAction() {
-    if(this.isPost()) {
-      var type = this.post();
-      console.log(type);
-
-      if(think.isEmpty(type.apiKey)) {
-          return false;
-      }
-
-      type.createTime = +new Date();
-
-      this.model('appinfo').add(type);
-      this.redirect('/admin/app');
+    /**
+     * index action
+     * @return {Promise} []
+     */
+    async indexAction() {
+        const data = await this.model('appinfo').select();
+        this.assign('data', data);
+        return this.display();
     }
 
+    addAction() {
+        if (this.isPost()) {
+            var type = this.post();
+            console.log(type);
 
-    var host = this.http.host;
-    var key = think.uuid();
+            if (think.isEmpty(type.apiKey)) {
+                return false;
+            }
 
-    this.assign('data',{key:key,embed:'<script src="http://'+host+'/tongji.js" data-apikey="'+key+'"></script>'});
+            type.createTime = +new Date();
 
-    return this.display();
-  }
+            this.model('appinfo').add(type);
+            this.redirect('/admin/app');
+        }
+
+
+        var host = this.http.host;
+        var key = think.uuid();
+
+        this.assign('data', {
+            key: key,
+            embed: '<script src="http://' + host + '/tongji.js" data-apikey="' + key + '"></script>'
+        });
+
+        return this.display();
+    }
 }
