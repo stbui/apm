566908bc094cd759da0d3c7b96f9fe98c50e7c8d
stbui <w431106@163.com>
2018-01-10 17:23:11 +0800

    u

--- apm-js-sdk/packages/core/rollup.config.js
+++ apm-js-sdk/packages/core/rollup.config.js
@@ -1,19 +1,38 @@
-const resolve = require('rollup-plugin-node-resolve');
-const sourcemaps = require('rollup-plugin-sourcemaps');
 
-const globals = {
-  '@apm/core': 'apm.core',
-  '@apm/common': 'apm.common',
-};
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+import resolve from 'rollup-plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
-module.exports = {
-  entry: './dist/packages/core/esm5/public_api.js',
-  dest: './dist/packages/core/bnundles/core.umd.js',
-  format: 'umd',
-  exports: 'named',
-  moduleName: 'apm.core',
-  plugins: [resolve(), sourcemaps()],
-  external: Object.keys(globals),
-  globals: globals,
-  banner: '/* test */'
+export default {
+  input: './dist/packages/core/esm5/public_api.js',
+  output: [
+    {
+      file: './dist/packages/core/bnundles/core.common.js',
+      format: 'cjs',
+      exports: 'named',
+    },
+    {
+      file: './dist/packages/core/bnundles/core.umd.js',
+      format: 'umd',
+      exports: 'named',
+      moduleName: 'apm.core',
+    },
+    {
+      file: './dist/packages/core/bnundles/core.esm.js',
+      format: 'es',
+      exports: 'named',
+    }
+  ],
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    // uglify(),
+    sourcemaps()
+  ],
+  banner: '/* apm-js-sdk */'
 };
