cf3fce5a236c53d97a16d84c1a818dff7988d195
stbui <w431106@163.com>
2018-01-12 10:48:15 +0800

    u

---
+++ apm-js-sdk/rollup.config.js
@@ -0,0 +1,36 @@
+import commonjs from "rollup-plugin-commonjs";
+import uglify from "rollup-plugin-uglify";
+import resolve from "rollup-plugin-node-resolve";
+import sourcemaps from "rollup-plugin-sourcemaps";
+
+export default {
+  input: "./dist/src/index.js",
+  output: [
+    {
+      file: "./dist/index.common.js",
+      format: "cjs",
+      exports: "named"
+    },
+    {
+      file: "./dist/index.umd.js",
+      format: "umd",
+      exports: "named",
+      moduleName: "ik.editor.drop"
+    },
+    {
+      file: "./dist/index.esm.js",
+      format: "es",
+      exports: "named"
+    }
+  ],
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true
+    }),
+    commonjs(),
+    // uglify(),
+    sourcemaps()
+  ]
+};
