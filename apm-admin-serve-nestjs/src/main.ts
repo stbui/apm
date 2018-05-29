b355abbfcdce34cbb23a036ac498b6e877fb494a
stbui <w431106@163.com>
2018-05-29 21:36:43 +0800

    style(serve): 支持静态文件访问

--- apm-admin-serve-nestjs/src/main.ts
+++ apm-admin-serve-nestjs/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/core/adapters/fastify-adapter';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  app.useStaticAssets({
+    root: join(__dirname, 'public'),
+    prefix: '/public/',
+  });
   await app.listen(3000);
 }
 bootstrap();
