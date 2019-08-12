fdd58ec4455217bcf8891337601b3e414fd0f30d
stbui <w431106@163.com>
2019-08-12 21:51:05 +0800

    feat: update

--- apm-js-sdk/packages/apm/src/config/config.ts
+++ apm-js-sdk/packages/apm/src/config/config.ts
@@ -81,3 +81,34 @@ export class Config {
     this._http = new ConfigHttp(config);
   }
 }
+
+export class configUrl {
+  DATA = 'http://127.0.0.1:3000/api/session/{id}/data?server_session_id={ssid}';
+  GET_IDENTITY_ROOT_DOMAIN =
+    'http://127.0.0.1:3000/api/identity/domain?host={host}';
+  PING = 'http://127.0.0.1:3000/api/session/{id}/ping?server_session_id={ssid}';
+  SESSION = 'http://127.0.0.1:3000/api/session';
+  SESSION_ACTIVE = 'http://127.0.0.1:3000/api/session/{id}/active';
+  SESSION_INITIAL_DATA = 'http://127.0.0.1:3000/api/session/{id}';
+  SETTINGS = 'http://127.0.0.1:3000/api/settings?url={url}';
+  SET_SESSION_IDENTITY = 'http://127.0.0.1:3000/api/session/{id}/identity';
+  UPDATE_SERVER_SESSION =
+    'http://127.0.0.1:3000/api/session/{id}/server_session/';
+
+  SHOULD_RESOLVE_SOURCE_MAPS = true;
+  VERSION = 61;
+  WEBSOCKET_URLS =
+    'wss://127.0.0.1:3000/api/session/{id}/data?server_session_id={ssid}';
+
+  buildHttpUrl(path) {
+    return this.serverUrl() + '/' + path;
+  }
+
+  formatUrl(a, b) {}
+
+  getServerUrl() {}
+
+  serverUrl() {
+    return '127.0.0.1:3000/api';
+  }
+}
