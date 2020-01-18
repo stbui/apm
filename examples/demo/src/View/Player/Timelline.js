7f5f77871303f496c5c68c5063dd6f8656b1c17e
stbui <w431106@163.com>
2020-01-18 22:38:13 +0800

    feat: wip

--- examples/demo/src/View/Player/Timelline.js
+++ examples/demo/src/View/Player/Timelline.js
@@ -13,11 +13,18 @@ export const Timelline = ({
     isLive,
     isCreated,
 }) => {
+    const wrapperRef = useRef();
     const handleRef = useRef();
     const clickableRef = useRef();
     const [state, setState] = useState({ value: 0 });
 
+    let offset;
+
     const getClickableWith = () => clickableRef.current.width;
+
+    const timelineValueToPercentage = value => {
+        return (value / offset) * 100;
+    };
 
     const setValue = currentValue => {
         let value = Math.max(currentValue, min);
@@ -31,25 +38,85 @@ export const Timelline = ({
         return min + calculated * 0;
     };
 
-    const getPercent = value => {
+    const clickableWidthToPercentage = value => {
         const width = getClickableWith();
         return (value / width) * 100;
     };
 
-    const f = currentValue => {
-        let value = (Math = Math.max(currentValue, 0));
-        value = Math.min(value, 100);
+    let activityBlocks = [];
+    let activityIndex = -1;
+    let percent = 0;
+    let timelineProgressBarWidth = 0;
+    let timelineProgressHandleOffset = 0;
+
+    const filterActivityBlocks = a => {
+        for (var b, c, d = [], e = offset / getClickableWith(), f = 0, g = a.length; f < g; )
+            for (b = a[f], d.push(b), c = b.time + e, f++; f < g && a[f].time < c; ) f++;
+        return d;
     };
 
-    const e = ({ pageX }) => {
+    const setPercent = state => {
+        if (activities[activityIndex]) {
+            percent = timelineValueToPercentage(activities[activityIndex].time);
+        } else if (state) {
+            percent = 100;
+        }
+    };
+
+    const updatetimelineProgress = _value => {
+        _value = Math.max(_value, 0);
+        _value = Math.min(_value, 100);
+
+        const _percent = timelineValueToPercentage(value);
+        timelineProgressBarWidth = Math.min(_percent, percent);
+        timelineProgressHandleOffset = _value - clickableWidthToPercentage(s / 2);
+    };
+
+    const timelineProgress = value => {
+        const percent = timelineValueToPercentage(value);
+        updatetimelineProgress(percent);
+    };
+
+    const onRefresh = state => {
+        if (Array.isArray(activities)) {
+            const newActivities = activities.slice(activityIndex + 1);
+            const _activityBlocks = activityBlocks;
+
+            newActivities.forEach(activity => {
+                _activityBlocks.push({ time: activity.time });
+            });
+
+            activityBlocks = filterActivityBlocks(_activityBlocks);
+            activityIndex = activities.length - 1;
+
+            setPercent(state);
+            timelineProgress(value);
+        }
+    };
+
+    const updateProgress = ({ pageX }) => {
         const { left } = clickableRef.current.offset();
         const offsetX = Math.max(pageX - left, min + 1);
         const value = getCalcValue(offsetX);
         setValue(value);
     };
 
+    const updateDirtyState = state => {
+        isDirty = state;
+    };
+
+    const onWrapperClick = event => {
+        updateDirtyState(true);
+        updateProgress(event);
+        updateDirtyState(false);
+    };
+
+    useEffect(() => {
+        offset = max - min;
+    }, [min, max]);
+
     return (
-        <div className="progress-wrapper">
+        <div className="progress-wrapper" ref={wrapperRef} onClick={onWrapperClick}>
             <div className="timeline-clickable-wrapper" ref={clickableRef}>
                 <div
                     draggable
