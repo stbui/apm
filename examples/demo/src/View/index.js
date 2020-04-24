9630b992d16ae19f1e707e5834edde0fe7fa0fea
stbui <w431106@163.com>
2020-04-24 22:46:00 +0800

    feat: wip

--- examples/demo/src/View/index.js
+++ examples/demo/src/View/index.js
@@ -10,21 +10,27 @@ const activities = mock.activities;
 
 let e = 0;
 export default () => {
-    return <Player />;
+    // return <Player />;
 
-    // const [a, b] = useState();
-    // const [aa, bb] = useState(false);
+    const [a, b] = useState();
+    const [aa, aaa] = useState(1);
 
-    // const c = () => {
-    //     bb(true);
-    // };
+    const c = () => {
+        let dd = 1;
+        setInterval(() => {
+            dd++;
+            e = 0;
+            aaa(dd);
+        }, 500);
+    };
 
-    // useEffect(() => {
-    //     console.log(e);
-    //     if (e < 10 && aa) {
-    //         b(e++);
-    //     }
-    // }, [e, aa]);
+    useEffect(() => {
+        if (e < 10 && aa !== 1) {
+            b(e++);
+        }
+    }, [e, aa]);
 
-    // return <button onClick={c}>ssssss{a}</button>;
+    console.log(e);
+
+    return <button onClick={c}>ssssss{e}</button>;
 };
